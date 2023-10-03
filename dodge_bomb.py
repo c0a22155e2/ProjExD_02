a515ef343a7c844d6ffdc044ffedff488be85f12
c0a22155e2 <c0a22155e2@edu.teu.ac.jp>
2023-10-03 15:10:55 +0900

    練習2終了

---
+++ dodge_bomb.py
@@ -0,0 +1,47 @@
+import sys
+import pygame as pg
+import random
+
+WIDTH, HEIGHT = 1600, 900
+
+
+def main():
+    pg.display.set_caption("逃げろ！こうかとん")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
+    kk_img = pg.image.load("ex02/fig/3.png")
+    kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    clock = pg.time.Clock()
+
+
+    bd_img = pg.Surface((20,20))
+    pg.draw.circle(bd_img , (255,0,0),(10,10),10)
+    bd_rct = bd_img.get_rect()
+    x,y = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    bd_rct.center = (x,y)
+
+    vx = 5
+    vy = 5
+
+
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: 
+                return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [900, 400])
+        screen.blit(bd_img, bd_rct)
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+
+        bd_rct.move_ip(vx,vy)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
